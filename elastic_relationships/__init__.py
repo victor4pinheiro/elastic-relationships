0baf28f3cf5a763ce80e2767908e79aa0b14f4f8
victor4pinheiro <victor.pinheiro.soft@proton.me>
2024-03-20 14:06:16 -0300

    WIP: add initial routes

--- elastic_relationships/__init__.py
+++ elastic_relationships/__init__.py
@@ -1,11 +1,27 @@
+from os import getenv
 from elasticsearch import Elasticsearch
 from flask import Flask, make_response
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+ELASTIC_USER = getenv("ELASTIC_USER") or "elastic"
+ELASTIC_PASSWORD = getenv("ELASTIC_PASSWORD") or "SuperSecret"
+ELASTIC_URL = getenv("ELASTIC_URL")
 
 app = Flask(__name__)
+db = Elasticsearch(
+        ELASTIC_URL,
+        basic_auth=(ELASTIC_USER, ELASTIC_PASSWORD),
+        verify_certs=False
+)
 
-@app.get("/")
-def main():
-    return make_response({ "message": "Main" }, 200)
+@app.post("/orders")
+def create_order():
+    
+    test = { "message": "Main" }
+    return make_response(test, 200)
 
 if __name__ == "__main__":
     app.run(debug=True)
